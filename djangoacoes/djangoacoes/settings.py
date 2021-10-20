c350d85f525a44d9f5a0f62e109b23c4188260f1
RicardoGoncal <ricardo.goncalves7@fatec.sp.gov.br>
2021-10-19 21:36:34 -0300

    Retirada chave secret django

--- djangoacoes/djangoacoes/settings.py
+++ djangoacoes/djangoacoes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m9-%kc9@y+7iv2l+aq3_@pypdx@466+3zo#ap%5rjp835x*+-1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
